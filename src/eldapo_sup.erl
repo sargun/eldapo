bbf8ea1f80cf76db456fce36abdc2e9234937d93
Sargun Dhillon <sargun@sargun.me>
2015-04-27 12:33:42 -0700

    Update supervisor

--- src/eldapo_sup.erl
+++ src/eldapo_sup.erl
@@ -10,7 +10,7 @@
 -export([init/1]).
 
 %% Helper macro for declaring children of supervisor
-% -define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
+-define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
 
 %% ===================================================================
 %% API functions
@@ -28,10 +28,11 @@ init([]) ->
     {ok, { {one_for_one, 5, 10},
             [
                 % first child tcp listner
-                { eldapo_tcp_acceptor,
-                    {eldapo_tcp_acceptor, start_link, []},
-                    permanent, brutal_kill, worker, []},
+%                { eldapo_tcp_acceptor,
+%                    {eldapo_tcp_acceptor, start_link, []},
+%                    permanent, brutal_kill, worker, []},
                 % second child eldapo handler supervisor
+                ?CHILD(ldap_schema, worker),
                 { eldapo_handler_sup,
                     {eldapo_handler_sup,start_link, []},
                     permanent, infinity, supervisor, []}
