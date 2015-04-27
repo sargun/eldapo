1b9f913a9da9bef27815ff7a87a1772681816449
Sargun Dhillon <sargun@sargun.me>
2015-04-27 12:34:50 -0700

    Add eldapo Listener

--- src/eldapo_app.erl
+++ src/eldapo_app.erl
@@ -10,6 +10,8 @@
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
+    {ok, _} = ranch:start_listener(ldap_handler, 100,
+      ranch_tcp, [{packet, asn1}, {port, 6360}], eldapo_fsm, [{backend, eldapo_backend}]),
     eldapo_sup:start_link().
 
 stop(_State) ->
